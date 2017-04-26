6673b2662500a820dc3d85ae8873cd919050fb77
Jim Geovedi <jim@geovedi.com>
2017-04-26 19:58:51 +0700

    style

--- scripts/lamtram-translate-splitchunks.py
+++ scripts/lamtram-translate-splitchunks.py
@@ -9,12 +9,18 @@ import plac
 
 
 class Segmenter(object):
-    SPLIT_AFTER = (', ', ') ', '} ', '] ', '| ', ' -- ', ': ', '; ', '. ', '? ', '! ', '" ', )
-    SPLIT_BEFORE = (' (', ' {', ' [', ' |', ' http://', ' https://', ' ftp://', )
+    SPLIT_AFTER = (', ', ') ', '} ', '] ', '| ', ' -- ', ': ', '; ', '. ',
+                   '? ', '! ', '" ', )
+    SPLIT_BEFORE = (' (', ' {', ' [', ' |', ' http://', ' https://',
+                    ' ftp://', )
 
     def __init__(self, code_fname, separator='￭'):
-        self.codes = [tuple(item.split()) for item in io.open(code_fname, encoding='utf-8')]
-        self.codes = dict([(code, i) for (i, code) in reversed(list(enumerate(self.codes)))])
+        self.codes = [
+            tuple(item.split()) for item in io.open(
+                code_fname, encoding='utf-8')
+        ]
+        self.codes = dict(
+            [(code, i) for (i, code) in reversed(list(enumerate(self.codes)))])
         self.separator = separator
         self.cache = {}
 
@@ -43,12 +49,20 @@ class Segmenter(object):
         text = re.sub(';', '; ', text)
         text = re.sub("' s", "'s ", text)
         text = re.sub(r'(\.\.+)', r' \1 ', text)
-        text = re.sub(r'([\p{L}\p{N}])([\p{P}\p{S}]+) ', r'\1 {}\2 '.format(self.separator), text)
-        text = re.sub(r' ([\p{P}\p{S}]+)([\p{L}\p{N}])', r' \1{} \2'.format(self.separator), text)
-        text = re.sub(r'([\p{L}\p{N}])([\p{P}\p{S}]+)([\p{L}\p{N}])', r'\1 {}\2{} \3'.format(self.separator, self.separator), text)
+        text = re.sub(r'([\p{L}\p{N}])([\p{P}\p{S}]+) ',
+                      r'\1 {}\2 '.format(self.separator), text)
+        text = re.sub(r' ([\p{P}\p{S}]+)([\p{L}\p{N}])',
+                      r' \1{} \2'.format(self.separator), text)
+        text = re.sub(r'([\p{L}\p{N}])([\p{P}\p{S}]+)([\p{L}\p{N}])',
+                      r'\1 {}\2{} \3'.format(self.separator,
+                                             self.separator), text)
         text = re.sub(r' ([\p{N}]+)([\p{L}])', r' \1 \2', text)
-        text = re.sub(r" {}'{} ([sSmMdD]|ll|re|ve|LL|RE|VE) ".format(self.separator, self.separator), r" {}'\1 ".format(self.separator), text)
-        text = re.sub(r"([Nn]) {}'{} ([Tt])".format(self.separator, self.separator), r" {}\1'\2 ".format(self.separator), text)
+        text = re.sub(r" {}'{} ([sSmMdD]|ll|re|ve|LL|RE|VE) ".format(
+            self.separator, self.separator), r" {}'\1 ".format(self.separator),
+                      text)
+        text = re.sub(r"([Nn]) {}'{} ([Tt])".format(self.separator,
+                                                    self.separator),
+                      r" {}\1'\2 ".format(self.separator), text)
         text = re.sub(r' +', ' ', text)
         text = re.sub(r'^ ', '', text)
         text = re.sub(r' $', '', text)
@@ -57,8 +71,10 @@ class Segmenter(object):
     def detokenize(self, text):
         text = re.sub(r'^', ' ', text)
         text = re.sub(r'$', ' ', text)
-        text = re.sub(r" {}'([sSmMdD]|ll|re|ve|LL|RE|VE) ".format(self.separator), r"'\1 ", text)
-        text = re.sub(r" {}([Nn])'([Tt]) ".format(self.separator), r"\1'\2 ", text)
+        text = re.sub(r" {}'([sSmMdD]|ll|re|ve|LL|RE|VE) ".format(
+            self.separator), r"'\1 ", text)
+        text = re.sub(r" {}([Nn])'([Tt]) ".format(self.separator), r"\1'\2 ",
+                      text)
         text = re.sub('{} {}'.format(self.separator, self.separator), '', text)
         text = re.sub('{} '.format(self.separator), '', text)
         text = re.sub(' {}'.format(self.separator), '', text)
@@ -98,7 +114,8 @@ class Segmenter(object):
                     new_word.extend(word[i:])
                     break
 
-                if word[i] == first and i < len(word) - 1 and word[i + 1] == second:
+                if word[i] == first and i < len(word) - 1 and word[
+                        i + 1] == second:
                     new_word.append(first + second)
                     i += 2
                 else:
@@ -121,8 +138,10 @@ class Segmenter(object):
         return word
 
     def punct_split(self, word):
-        word = re.sub(r'^([\p{P}]+)([\p{L}\p{N}]+)', r'\1{} \2'.format(self.separator), word)
-        word = re.sub(r'([\p{L}\p{N}]+)([\p{P}]+)$', r'\1 {}\2'.format(self.separator), word)
+        word = re.sub(r'^([\p{P}]+)([\p{L}\p{N}]+)',
+                      r'\1{} \2'.format(self.separator), word)
+        word = re.sub(r'([\p{L}\p{N}]+)([\p{P}]+)$',
+                      r'\1 {}\2'.format(self.separator), word)
         return word
 
     def split(self, sent, max_length=50):
@@ -158,16 +177,23 @@ class Segmenter(object):
         return chunks
 
 
-def main(model, codes, input_fname, output_fname, detokenize=False, segment_length=20):
+def main(model,
+         codes,
+         input_fname,
+         output_fname,
+         detokenize=False,
+         segment_length=20,
+         dynet_mem=1000):
     segmenter = Segmenter(codes)
     lamtram_cmd = '''
         lamtram
-            --dynet-mem 1000
-            --models_in encatt={0}
+            --dynet-mem {0}
+            --models_in encatt={1}
             --operation gen
-    '''.format(model).split()
+    '''.format(dynet_mem, model).split()
 
-    proc = subprocess.Popen(lamtram_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    proc = subprocess.Popen(
+        lamtram_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     with io.open(output_fname, 'w', buffering=1, encoding='utf-8') as out:
         for line in io.open(input_fname, 'r', encoding='utf-8'):

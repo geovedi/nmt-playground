7cc4c000f94999b92bf851b50c236198be7a26b5
Jim Geovedi <jim@geovedi.com>
2017-06-05 14:13:44 +0700

    use subprocess instead libamunmt
    nmt.ini() keep complaining about existing logger

--- scripts/marian-ensemble-find-best.py
+++ scripts/marian-ensemble-find-best.py
@@ -4,9 +4,16 @@ import io
 import uuid
 import subprocess
 import fire
-import libamunmt as nmt
+import importlib
 from pyaml import yaml
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
+
 
 def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
     if not os.path.exists(workdir):
@@ -16,24 +23,20 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
 
     def translator(weights):
         exp_id = str(uuid.uuid1())
-        print('Start experiment: id: "{0}", weights: {1}'
-              .format(exp_id, weights))
+        logging.info('Start experiment: id: "{0}", weights: {1}'
+                     .format(exp_id, weights))
 
         exp_config = '{0}/{1}.yml'.format(workdir, exp_id)
         config['weights'] = weights
         with io.open(exp_config, 'w', encoding='utf-8') as out:
             out.write(yaml.dump(config))
-        nmt.init('-c {0} --log-progress=0 --log-info=0'.format(exp_config))
 
         exp_out = '{0}/{1}.out'.format(workdir, exp_id)
-        with io.open(exp_out, 'w', encoding='utf-8') as out:
-            for line in io.open(valid_src, 'r', encoding='utf-8'):
-                text = line.strip()
-                if text:
-                    trans = nmt.translate([text])[0]
-                else:
-                    trans = ''
-                out.write('{0}\n'.format(trans))
+        with io.open(valid_src, 'r', encoding='utf-8') as infile, \
+             io.open(exp_out, 'w', encoding='utf-8') as outfile:
+            cmd = ['amun', '-c', exp_config]
+            proc = subprocess.Popen(cmd, stdin=infile, stdout=outfile)
+            proc.wait()
 
         with io.open(exp_out, 'r', encoding='utf-8') as infile:
             cmd = ['run-scorer', 'BLEU', 'case:1', valid_tgt]
@@ -41,8 +44,8 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
         proc_out, proc_err = proc.communicate()
         score = float(proc_out)
 
-        print('End experiment: id: {0}, score: {1}'
-              .format(exp_id, score))
+        logging.info('End experiment: id: {0}, score: {1}'
+                     .format(exp_id, score))
         loss = 1.0 - score
         return {'loss': loss, 'status': STATUS_OK}
 
@@ -60,7 +63,7 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
         max_evals=max_evals,
         trials=trials)
 
-    print('Best: {0}'.format(best))
+    logging.info('Best: {0}'.format(best))
 
 
 if __name__ == '__main__':

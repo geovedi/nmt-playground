49740cc79efd39971d4a58ed639311400d06b5b1
Jim Geovedi <jim@geovedi.com>
2017-06-05 13:41:27 +0700

    Update marian-ensemble-find-best.py

--- scripts/marian-ensemble-find-best.py
+++ scripts/marian-ensemble-find-best.py
@@ -4,15 +4,9 @@ import io
 import uuid
 import subprocess
 import fire
+import libamunmt as nmt
 from pyaml import yaml
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-
-import logging
-logging.basicConfig(
-    format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    level=logging.INFO)
-
 
 def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
     if not os.path.exists(workdir):
@@ -22,16 +16,15 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
 
     def translator(weights):
         exp_id = str(uuid.uuid1())
-        config['weights'] = weights
-        logging.info('Start experiment: id: "{0}", weights: {1}'
-                      .format(exp_id, weights))
+        print('Start experiment: id: "{0}", weights: {1}'
+              .format(exp_id, weights))
 
         exp_config = '{0}/{1}.yml'.format(workdir, exp_id)
+        config['weights'] = weights
         with io.open(exp_config, 'w', encoding='utf-8') as out:
             out.write(yaml.dump(config))
-
-        import libamunmt as nmt
         nmt.init('-c {0}'.format(exp_config))
+
         exp_out = '{0}/{1}.out'.format(workdir, exp_id)
         with io.open(exp_out, 'w', encoding='utf-8') as out:
             for line in io.open(valid_src, 'r', encoding='utf-8'):
@@ -48,15 +41,15 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
         proc_out, proc_err = proc.communicate()
         score = float(proc_out)
 
-        logging.info('End experiment: id: {0}, score: {1}'
-                     .format(exp_id, score))
+        print('End experiment: id: {0}, score: {1}'
+              .format(exp_id, score))
         loss = 1.0 - score
         return {'loss': loss, 'status': STATUS_OK}
 
     def create_space(weights):
         space = {}
         for key in weights.keys():
-            space[key] = hp.uniform(key, 0, 1)
+            space[key] = hp.uniform(key, -1, 1)
         return space
 
     trials = Trials()
@@ -67,7 +60,7 @@ def main(workdir, base_config, valid_src, valid_tgt, max_evals=100):
         max_evals=max_evals,
         trials=trials)
 
-    logging.info('Best: {0}'.format(best))
+    print('Best: {0}'.format(best))
 
 
 if __name__ == '__main__':

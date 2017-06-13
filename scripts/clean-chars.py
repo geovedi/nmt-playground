9452a135d8a3f23df85021ddc067fb2f3aab06a9
Jim Geovedi <jim@geovedi.com>
2017-06-13 15:09:33 +0700

    added customised bpe tool and character cleaner

---
+++ scripts/clean-chars.py
@@ -0,0 +1,30 @@
+import io
+import fire
+import re
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO)
+
+UNWANTED = re.compile('[^'
+    '\u0000-\u007f\u0080-\u00ff\u0100-\u017F\u1E00-\u1EFF\u0180-\u024F'
+    '\u2C60-\u2C7F\uA720-\uA7FF\uAB30-\uAB6F'
+    '\u2014\u0024\u00a2-\u00a5\u0e3f\u20a0\u20a1\u20a3-\u20a5\u20ac'
+    '\uffe1\uffe5\u20b9'
+    ']', re.UNICODE)
+
+
+def main(input, output):
+    with io.open(output, 'w', encoding='utf-8') as out:
+        for line_no, line in enumerate(io.open(input, 'r', encoding='utf-8')):
+            if UNWANTED.search(line):
+                continue
+            out.write(line)
+            if line_no % 10000 == 0:
+                logging.info('Processed {0} sentences.'.format(line_no))
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
